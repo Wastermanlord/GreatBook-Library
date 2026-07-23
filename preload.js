066f70c56340b63573b182fdc446e1c74d5b2d99
Wastermanlord <wastermanlord@users.noreply.github.com>
2026-07-22 23:07:36 -0700

    fix: empty chapter-nav en fanfic_1_002, aria-expanded en descargas, nav completo en 404

--- preload.js
+++ preload.js
@@ -7,15 +7,19 @@ contextBridge.exposeInMainWorld('electronAPI', {
   downloadUpdate: () => ipcRenderer.send('download-update'),
   installUpdate: () => ipcRenderer.send('install-update'),
   onUpdateStatus: (cb) => {
+    ipcRenderer.removeAllListeners('update-status')
     ipcRenderer.on('update-status', (_event, status) => cb(status))
   },
   onUpdateInfo: (cb) => {
+    ipcRenderer.removeAllListeners('update-info')
     ipcRenderer.on('update-info', (_event, info) => cb(info))
   },
   onDownloadProgress: (cb) => {
+    ipcRenderer.removeAllListeners('download-progress')
     ipcRenderer.on('download-progress', (_event, progress) => cb(progress))
   },
   onUpdateError: (cb) => {
+    ipcRenderer.removeAllListeners('update-error')
     ipcRenderer.on('update-error', (_event, msg) => cb(msg))
   },
 })
