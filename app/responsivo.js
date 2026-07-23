50353532502f35bd42fb5a993b7900b7b6a64310
Wastermanlord <wastermanlord@users.noreply.github.com>
2026-07-23 00:21:33 -0700

    fix: service worker try/catch para Electron dev

--- app/responsivo.js
+++ app/responsivo.js
@@ -1,6 +1,6 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js')
-}
+try {
+  if ('serviceWorker' in navigator) navigator.serviceWorker.register('sw.js')
+} catch {}
 
 document.addEventListener('DOMContentLoaded', function() {
 
